fb1b8ba1ef05f4f7aa98b467b937100e4c9c827d
beckd <dbeck2@outlook.com>
2019-08-17 13:13:43 -0700

    fixing access error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var methodOverride = require('method-override');
 var cors = require('cors');
 
 // Configuration
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/users");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/users");
 
 app.use(bodyParser.urlencoded({ 'extended': 'true' }));
 app.use(bodyParser.json());
