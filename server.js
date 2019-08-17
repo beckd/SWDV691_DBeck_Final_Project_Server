d9ff03a6010ce66f5bc9773e7d587673ec6bebe3
beckd <dbeck2@outlook.com>
2019-08-17 14:16:31 -0700

    one more time...

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ 'extended': 'true' }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
-app.use(cors());
+//app.use(cors());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
